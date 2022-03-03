4a655778e716b109c813460dfa5da744c32bccf4
NullKtDev <glenyv@protonmail.com>
2022-03-03 20:07:45 +0100

    Added file for pure Nostr tests. To add tests later.

--- kostr-core/src/test/kotlin/NostrTests.kt
+++ kostr-core/src/test/kotlin/NostrTests.kt
@@ -1,4 +1,30 @@
+import org.junit.jupiter.api.Test
+
 class NostrTests {
 
+    @Test
+    fun `it correctly generates the event id`(){
+
+    }
+
+    @Test
+    fun `it generates a correct event`(){
+
+    }
+
+    @Test
+    fun `it can verify an event`(){
+
+    }
+
+    @Test
+    fun `it can generate a subscription event`(){
+
+    }
+
+    @Test
+    fun `it can correctly parse a received event`(){
+
+    }
 
 }
