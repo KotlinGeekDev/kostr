8259bc743068ee4f247f112a3d87b38e9d198897
NullKtDev <glenyv@protonmail.com>
2022-03-03 19:59:33 +0100

    Some Gradle modifications for testing purposes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,12 @@ buildscript {
 
     dependencies {
 
-
         classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        //classpath("")
 
     }
 }
 
-plugins {
-    kotlin("jvm") version "1.5.31"
-}
 
 allprojects {
     repositories {
@@ -23,21 +19,8 @@ allprojects {
         mavenCentral()
     }
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
-        kotlinOptions.jvmTarget = "1.8"
-    }
 
     group = "tk.anonymousgeek"
     version = "1.0-SNAPSHOT"
 }
-dependencies {
-   // implementation("junit:junit:4.13.1")
-}
 
-//tasks.register<Delete>("clean"){
-//    delete(rootProject.buildDir)
-//}
-
-repositories {
-    mavenCentral()
-}
--- kostr-core/build.gradle.kts
+++ kostr-core/build.gradle.kts
@@ -1,30 +1,29 @@
+
 plugins {
     `java-library`
-   id("kotlin")
+   kotlin("jvm") version "1.5.31"
 
 }
 
 group = "kt.nostr"
 version = "1.0-SNAPSHOT"
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
 
-
-//java {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-//}
 
 dependencies {
+
     //  implementation("fr.acinq.secp256k1:secp256k1-kmp-jvm:0.6.1")
     //  implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-common:0.6.1")
     implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-linux:0.6.2")
     implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-extract:0.6.2")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    //implementation("org.junit.jupiter:junit-jupiter:5.8.2")
     // implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm:0.6.1")
 
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
