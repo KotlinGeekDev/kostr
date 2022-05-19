3a9df61f59471b5b548b5657715e5392dfcb1bf8
NullKtDev <glenyufui@gmail.com>
2022-05-19 16:58:09 +0100

    Fix RelayMessage Json conversion(finally!) and add initial tests.

--- kostr-core/src/main/kotlin/ktnostr/nostr/relays/RelayMessage.kt
+++ kostr-core/src/main/kotlin/ktnostr/nostr/relays/RelayMessage.kt
@@ -2,40 +2,61 @@
 
 package ktnostr.nostr.relays
 
+//import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.module.kotlin.readValue
-import ktnostr.nostr.RelayMessageError
-import ktnostr.nostr.eventMapper
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.node.ArrayNode
 
-@JsonSubTypes(
-    JsonSubTypes.Type(RelayEventMessage::class),
-    JsonSubTypes.Type(RelayNotice::class)
-)
+@JsonDeserialize(using = RelayMessageConverter::class)
 sealed class RelayMessage
 
 @JsonFormat(shape = JsonFormat.Shape.ARRAY)
 data class RelayEventMessage(val messageType: String, val subscriptionId: String,
-                                val eventJson: String): RelayMessage()
+                             val eventJson: String): RelayMessage()
 
 @JsonFormat(shape = JsonFormat.Shape.ARRAY)
 data class RelayNotice(val messageType: String,
                        val message: String): RelayMessage()
 
-//This function is kept for legacy and last-resort purposes.
-fun String.toRelayMessage(): RelayMessage {
-    val json = this
-    val eventMessageRaw = eventMapper.readValue<List<String>>(json)
-    if (eventMessageRaw.size > 3 || eventMessageRaw.size < 2){
-        println("Unsupported relay message")
-        throw RelayMessageError("This relay message cannot be understood.\n Message: $eventMessageRaw")
-    }
-    else {
-        return when(eventMessageRaw.first()){
-            "EVENT" -> RelayEventMessage(eventMessageRaw[0], eventMessageRaw[1], eventMessageRaw[2])
-            "NOTICE" -> RelayNotice(eventMessageRaw[0], eventMessageRaw[1])
-            else -> throw RelayMessageError("This message does not conform to the standard format of" +
-                    "relay messages. \n MessageJson: $this")
+class RelayMessageConverter: JsonDeserializer<RelayMessage>() {
+
+    override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): RelayMessage {
+        val messageTree = p?.readValueAsTree<ArrayNode>()
+        if (messageTree?.size()!! > 3 || messageTree.size() < 2) {throw Exception("This message type is not supported.")}
+
+
+        if (messageTree.size() == 3){
+            val messageType = messageTree[0].asText()
+            val subscriptionId = messageTree[1].asText()
+            val content = if (messageTree[2].isContainerNode) messageTree[2].toString() else ""
+            return RelayEventMessage(messageType, subscriptionId, content)
+        } else {
+            val messageType = messageTree[0].asText()
+            val message = messageTree[1].asText()
+            return RelayNotice(messageType, message)
         }
     }
+
 }
+
+
+//----This function is kept for legacy and last-resort purposes----
+//fun String.toRelayMessage(): RelayMessage {
+//    val json = this
+//    val eventMessageRaw = eventMapper.readValue<List<String>>(json)
+//    if (eventMessageRaw.size > 3 || eventMessageRaw.size < 2){
+//        println("Unsupported relay message")
+//        throw RelayMessageError("This relay message cannot be understood.\n Message: $eventMessageRaw")
+//    }
+//    else {
+//        return when(eventMessageRaw.first()){
+//            "EVENT" -> RelayEventMessage(eventMessageRaw[0], eventMessageRaw[1], eventMessageRaw[2])
+//            "NOTICE" -> RelayNotice(eventMessageRaw[0], eventMessageRaw[1])
+//            else -> throw RelayMessageError("This message does not conform to the standard format of" +
+//                    "relay messages. \n MessageJson: $this")
+//        }
+//    }
+//}
--- kostr-core/src/test/kotlin/RelayMessageTests.kt
+++ kostr-core/src/test/kotlin/RelayMessageTests.kt
@@ -1,6 +1,6 @@
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import ktnostr.nostr.eventMapper
+import ktnostr.nostr.relays.RelayEventMessage
 import ktnostr.nostr.relays.RelayMessage
 import ktnostr.nostr.relays.RelayNotice
 import org.junit.jupiter.api.Test
@@ -8,17 +8,31 @@ import org.junit.jupiter.api.Test
 class RelayMessageTests {
 
     @Test
-    fun `the relay event message is correctly parsed`(){
+    fun `the relay message is correctly parsed to a notice`(){
         val relayNoticeJson = """["NOTICE", "You are not allowed to publish to this relay"]"""
-        val relayNotice: RelayMessage = eventMapper.readValue<RelayNotice>(relayNoticeJson)
+        val relayNotice = testEventMapper.readValue<RelayMessage>(relayNoticeJson)
         val correctRelayNotice = RelayNotice("NOTICE",
             "You are not allowed to publish to this relay")
-        val correctRelayNoticeJson = eventMapper.writeValueAsString(correctRelayNotice)
+        val correctRelayNoticeJson = testEventMapper.writeValueAsString(correctRelayNotice)
         println(correctRelayNoticeJson)
         println(relayNotice)
         println(correctRelayNotice)
         assert(relayNotice == correctRelayNotice)
 
+    }
+
+    @Test
+    fun `the relay message is correctly parsed to a relay message event`(){
+        val relayMessageJson = """["EVENT","mySub",{"id":"f0d7e34a1f531c04fee5846ee85ae564e9e0ed389e82fd79c58f2bedca19b0e4","pubkey":"056ccc33d638633ecc60ee28db5f226ad3acfdfe69a73aa9670cc8beb0d9dc74","created_at":1640340342,"kind":0,"tags":[],"content":"{\"name\":\"Colby\",\"picture\":\"https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/12/0/0/Bitcoin-Gold.jpg?ve=1&tl=1\",\"about\":\"Testing\"}","sig":"f0fa211543b32adf6892ee5f18f85e70946dc1579fd390b04d12579d919d9afbb5195395e8b3ccb3c2d03fa21184c0935c322151b584152cbc3c89b2048f8442"}]"""
+        val relayMessage = testEventMapper.readValue<RelayMessage>(relayMessageJson)
+        val correctRelayMessage = RelayEventMessage("EVENT",
+            "mySub", "{\"id\":\"f0d7e34a1f531c04fee5846ee85ae564e9e0ed389e82fd79c58f2bedca19b0e4\",\"pubkey\":\"056ccc33d638633ecc60ee28db5f226ad3acfdfe69a73aa9670cc8beb0d9dc74\",\"created_at\":1640340342,\"kind\":0,\"tags\":[],\"content\":\"{\\\"name\\\":\\\"Colby\\\",\\\"picture\\\":\\\"https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/12/0/0/Bitcoin-Gold.jpg?ve=1&tl=1\\\",\\\"about\\\":\\\"Testing\\\"}\",\"sig\":\"f0fa211543b32adf6892ee5f18f85e70946dc1579fd390b04d12579d919d9afbb5195395e8b3ccb3c2d03fa21184c0935c322151b584152cbc3c89b2048f8442\"}"
+        )
+        val correctEventMessageJson = testEventMapper.writeValueAsString(correctRelayMessage)
+        println(correctEventMessageJson)
+        println(relayMessage)
+        println(correctRelayMessage)
+        assert(relayMessage == correctRelayMessage)
     }
 
 }
