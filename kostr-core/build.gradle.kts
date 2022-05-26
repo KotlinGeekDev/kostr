a51ba539991589734a56fdd0192e6e6837143f02
NullKtDev <glenyufui@gmail.com>
2022-05-26 14:03:48 +0100

    Re-arrange gradle config, and add new dependencies.

--- kostr-core/build.gradle.kts
+++ kostr-core/build.gradle.kts
@@ -1,3 +1,6 @@
+ val scarletVersion = "0.1.12"
+ val kotlinVersion = "1.5.31"
+ val jacksonVersion = "2.13.3"
 
 plugins {
     `java-library`
@@ -17,14 +20,14 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
 
 dependencies {
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${properties["version.kotlin"]}")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${properties["version.kotlin"]}")
-    implementation("com.tinder.scarlet:scarlet:${properties["version.scarlet"]}")
-    implementation("com.tinder.scarlet:websocket-okhttp:${properties["version.scarlet"]}")
-    implementation("com.tinder.scarlet:stream-adapter-built-in:${properties["version.scarlet"]}")
-    implementation("com.tinder.scarlet:message-adapter-jackson:${properties["version.scarlet"]}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("com.tinder.scarlet:scarlet:${scarletVersion}")
+    implementation("com.tinder.scarlet:websocket-okhttp:${scarletVersion}")
+    implementation("com.tinder.scarlet:stream-adapter-built-in:${scarletVersion}")
+    implementation("com.tinder.scarlet:message-adapter-jackson:${scarletVersion}")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:${properties["version.jackson"]}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
       implementation("fr.acinq.secp256k1:secp256k1-kmp-jvm:0.6.4")
     //  implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-common:0.6.1")
     //implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-extract:0.6.2")
@@ -33,17 +36,23 @@ dependencies {
 
     implementation(kotlin("stdlib"))
    // implementation("com.squareup.moshi:moshi:1.13.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${properties["version.jackson"]}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-test-junit:${kotlinVersion}")
+    testImplementation("junit:junit:4.13.2")
+
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
     runtimeOnly("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-linux:0.6.4")
     testRuntimeOnly("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-linux:0.6.4")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
 
 }
 
 
 
-tasks.getByName<Test>("test") {
+tasks.test {
     useJUnitPlatform()
 }
 
--- kostr-core/src/test/kotlin/EventTests.kt
+++ kostr-core/src/test/kotlin/EventTests.kt
@@ -1,8 +1,11 @@
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import ktnostr.nostr.Tag
 import ktnostr.nostr.rawEventJson0
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
+internal val testEventMapper = jacksonObjectMapper()
 class EventTests {
 
     @Test
@@ -18,8 +21,26 @@ class EventTests {
                             someTags, rawEventData[3])
         val correctRawJson = "[0,\"8565b1a5a63ae21689b80eadd46f6493a3ed393494bb19d0854823a757d8f35f\",1649108200,1,[[\"#p\",\"42365g3ghgf7gg15hj64jk\"]],\"Testing some event\"]"
         println(rawEventInJson)
-        assert(rawEventInJson == correctRawJson)
+        assertEquals(rawEventInJson, correctRawJson)
 
+    }
+
+    @Test
+    fun `it generates correct events with multiple tags`(){
+//        val eventTestTag = listOf(
+//            Tag("p", "13adc511de7e1cfcf1c6b7f6365fb5a03442d7bcacf565ea57fa7770912c023d"),
+//        )
+//        val rawEventTestData = listOf("f86c44a2de95d9149b51c6a29afeabba264c18e2fa7c49de93424a0c56947785",
+//            "1640839235", "4", "uRuvYr585B80L6rSJiHocw==?iv=oh6LVqdsYYol3JfFnXTbPA==",
+//            "a5d9290ef9659083c490b303eb7ee41356d8778ff19f2f91776c8dc4443388a64ffcf336e61af4c25c05ac3ae952d1ced889ed655b67790891222aaa15b99fdd")
+//        val eventId = getEventId(rawEventTestData[0], rawEventTestData[1].toLong(),
+//            rawEventTestData[2].toInt(), eventTestTag, rawEventTestData[3])
+//        val testEvent = Event(eventId, rawEventTestData[0], rawEventTestData[1].toLong(),
+//            rawEventTestData[2].toInt(), eventTestTag, rawEventTestData[3], rawEventTestData[4])
+//        val eventJson = testEvent.serialize()
+//
+//        println(eventJson)
+//        assert(true)
     }
 
 
