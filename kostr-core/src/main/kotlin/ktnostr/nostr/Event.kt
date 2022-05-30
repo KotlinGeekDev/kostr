e7fe2125d460129cde04fa250c2d685e4cc1532f
NullKtDev <glenyufui@gmail.com>
2022-05-30 11:22:18 +0100

    Add some documentation to EventKind.

--- kostr-core/src/main/kotlin/ktnostr/nostr/Event.kt
+++ kostr-core/src/main/kotlin/ktnostr/nostr/Event.kt
@@ -106,11 +106,35 @@ internal fun rawEventJson0(pubkey: String, timeStamp: Long, eventKind: Int,
  * currently used on Nostr. Not all are supported, though.
  */
 object EventKind {
+    /**
+     * Represents profile creation and modification on Nostr.
+     */
     const val METADATA = 0
+
+    /**
+     * Represents published notes or posts or 'tweets'.
+     */
     const val TEXT_NOTE = 1
+
+    /**
+     * Represents relay recommendations, for sharing relays. For helping with censorship-resistance.
+     */
     const val RELAY_RECOMMENDATION = 2
+
+    /**
+     * Represents contact lists, for sharing profiles, and (probably) building
+     * friend lists.
+     */
     const val CONTACT_LIST = 3
+
+    /**
+     * Represents encrypted messages.
+     */
     const val ENCRYPTED_DM = 4
+
+    /**
+     * Represents posts marked for deletion.
+     */
     const val MARKED_FOR_DELETION = 5
 
     @JvmStatic
