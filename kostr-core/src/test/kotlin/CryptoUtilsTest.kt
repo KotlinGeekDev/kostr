4910e2709468be92c1975aa9176dddee2a114ca7
Leo Wandersleb <leo@leowandersleb.de>
2022-06-05 14:36:08 -0400

    unit test cleanup

--- kostr-core/src/test/kotlin/CryptoUtilsTest.kt
+++ kostr-core/src/test/kotlin/CryptoUtilsTest.kt
@@ -1,37 +1,27 @@
 import fr.acinq.secp256k1.Hex
+import fr.acinq.secp256k1.Secp256k1
 import ktnostr.crypto.CryptoUtils
 import ktnostr.crypto.toHexString
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import kotlin.test.assertEquals
 
 
 class CryptoUtilsTest {
-
-    //    @Test
-//    fun `it needs to detect invalid seckeys when creating pubkeys`(){
-//        val context = Secp256k1.get()
-//
-//        val error = assertThrows<IllegalArgumentException>("Invalid seckey"){
-//            context.secKeyVerify(Hex.decode("6ba903b7888191180a0959a6d286b9d0719d33a47395c519ba107470412d2"))
-//        }
-//        assert("Failed requirement." == error.message)
-//
-//    }
     @Test
     fun testGetPublicKey() {
-        val testPublicKey =
-            CryptoUtils.getPublicKey(Hex.decode("6ba903b7888191180a0959a6d286b9d0719d33a47395c519ba107470412d2069"))
-        assert(Hex.encode(testPublicKey) == "8565b1a5a63ae21689b80eadd46f6493a3ed393494bb19d0854823a757d8f35f") {
-            "Pubkeys do not match!"
-        }
+        val secKeyHex = "6ba903b7888191180a0959a6d286b9d0719d33a47395c519ba107470412d2069"
+        val pubKeyHex = "8565b1a5a63ae21689b80eadd46f6493a3ed393494bb19d0854823a757d8f35f"
+        val secKeyBytes = Hex.decode(secKeyHex)
+        val pubKeyBytes = CryptoUtils.getPublicKey(secKeyBytes)
+        val pubKeyHexActual = Hex.encode(pubKeyBytes)
+        assertEquals(pubKeyHex, pubKeyHexActual, "PubKeys do not match!")
     }
 
     @Test
     fun testContentHash() {
-        val word = "Kotlin"
-        val correctHash = "c78f6c97923e81a2f04f09c5e87b69e085c1e47066a1136b5f590bfde696e2eb"
-
-        assert(CryptoUtils.contentHash(word).toHexString() == correctHash) {
-            "Hashes do not match!"
-        }
+        val content = "Kotlin"
+        val hashHex = "c78f6c97923e81a2f04f09c5e87b69e085c1e47066a1136b5f590bfde696e2eb"
+        val hashActualHex = CryptoUtils.contentHash(content).toHexString()
+        assertEquals(hashHex, hashActualHex, "Hashes do not match!")
     }
 }
