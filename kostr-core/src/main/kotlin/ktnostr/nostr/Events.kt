b50d02897687131ebf68360a985e84812b2a32c7
NullKtDev <glenyufui@gmail.com>
2022-06-23 17:52:41 +0100

    Add more event APIs. Minor tweaks.

--- kostr-core/src/main/kotlin/ktnostr/nostr/Events.kt
+++ kostr-core/src/main/kotlin/ktnostr/nostr/Events.kt
@@ -8,7 +8,7 @@ import ktnostr.currentSystemTimestamp
 
 object Events {
 
-    fun generateEvent(eventKind: Int,
+    internal fun generateEvent(eventKind: Int,
                       tags: List<Tag>,
                       content: String,
                       privateKeyHex: String, publicKeyHex: String): Event {
@@ -23,29 +23,53 @@ object Events {
     }
 
     @JvmStatic
-    fun metadataEvent(privkey: String,
+    fun MetadataEvent(privkey: String,
                       pubkey: String,
                       tags: List<Tag> = emptyList(),
-                      kind: Int = EventKind.METADATA, content: String): Event {
+                      kind: Int = EventKind.METADATA, profile: String): Event {
 
-        return generateEvent(kind, tags, content, privkey, pubkey)
+        return generateEvent(kind, tags, profile, privkey, pubkey)
     }
 
     @JvmStatic
-    fun textEvent(privkey: String, pubkey: String,
+    fun TextEvent(privkey: String, pubkey: String,
                   tags: List<Tag> = emptyList(),
                   kind: Int = EventKind.TEXT_NOTE, content: String): Event {
         return generateEvent(kind, tags, content, privkey, pubkey)
     }
 
     @JvmStatic
-    fun relayRecommendationEvent(privkey: String, pubkey: String,
+    fun RelayRecommendationEvent(privkey: String, pubkey: String,
                                  tags: List<Tag> = emptyList(),
                                  kind: Int = EventKind.RELAY_RECOMMENDATION,
                                  content: String): Event {
-        if (!content.startsWith("wss") || !content.startsWith("ws")) {
+        if (!(content.startsWith("wss") || content.startsWith("ws"))) {
             throw EventValidationError("Content $content is not a valid relay URL.")
             }
+        return generateEvent(kind, tags, content, privkey, pubkey)
+    }
+
+    @JvmStatic
+    fun FollowEvent(privkey: String, pubkey: String,
+                    tags: List<Tag>,
+                    kind: Int = EventKind.CONTACT_LIST,
+                    content: String): Event {
+        return generateEvent(kind, tags, content, privkey, pubkey)
+    }
+
+    @JvmStatic
+    fun DirectMessageEvent(privkey: String, pubkey: String,
+                           tags: List<Tag> = emptyList(),
+                           kind: Int = EventKind.ENCRYPTED_DM,
+                           content: String): Event {
+        return generateEvent(kind, tags, content, privkey, pubkey)
+    }
+
+    @JvmStatic
+    fun DeletionEvent(privkey: String, pubkey: String,
+                      tags: List<Tag> = emptyList(),
+                      kind: Int = EventKind.MARKED_FOR_DELETION,
+                      content: String): Event {
         return generateEvent(kind, tags, content, privkey, pubkey)
     }
 
