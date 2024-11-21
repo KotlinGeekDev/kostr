28d5aebdfcca0279617240de1e186d2526190c5d
KotlinGeekDev <kotlingeek@protonmail.com>
2024-11-21 14:15:19 +0100

    Aside: Make visibility changes.

--- kostr-core/src/appleMain/kotlin/ktnostr/net/HttpClient.apple.kt
+++ kostr-core/src/appleMain/kotlin/ktnostr/net/HttpClient.apple.kt
@@ -3,7 +3,7 @@ package ktnostr.net
 import io.ktor.client.*
 import io.ktor.client.engine.darwin.*
 
-actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(Darwin) {
+internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(Darwin) {
     config(this)
     engine {
         
--- kostr-core/src/jvmMain/kotlin/ktnostr/net/HttpClient.jvm.kt
+++ kostr-core/src/jvmMain/kotlin/ktnostr/net/HttpClient.jvm.kt
@@ -3,7 +3,7 @@ package ktnostr.net
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 
-actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(OkHttp) {
+internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(OkHttp) {
     config(this)
 
 }
