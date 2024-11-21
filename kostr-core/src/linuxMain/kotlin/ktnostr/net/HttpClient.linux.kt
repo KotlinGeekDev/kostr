6f5bcfea6123b031a7940e069ab402e79caf04a9
KotlinGeekDev <kotlingeek@protonmail.com>
2024-11-21 14:15:19 +0100

    Aside: Make visibility changes.

--- kostr-core/src/linuxMain/kotlin/ktnostr/net/HttpClient.linux.kt
+++ kostr-core/src/linuxMain/kotlin/ktnostr/net/HttpClient.linux.kt
@@ -3,7 +3,7 @@ package ktnostr.net
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 
-actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(CIO) {
+internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(CIO) {
     config(this)
     engine {
 
