c7cb90d431ff86273244c0f200b77071f55cee14
KotlinGeekDev <kotlingeek@protonmail.com>
2025-03-15 21:56:30 +0100

    Initial support for NIP-11.

--- rhodium-core/src/commonMain/kotlin/rhodium/nostr/relay/Relay.kt
+++ rhodium-core/src/commonMain/kotlin/rhodium/nostr/relay/Relay.kt
@@ -1,5 +1,15 @@
 package rhodium.nostr.relay
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import rhodium.net.httpClient
+import rhodium.nostr.RelayInfoFetchError
+import rhodium.nostr.eventMapper
+
 class Relay(
     val relayURI: String,
     val readPolicy: Boolean = true,
@@ -9,9 +19,40 @@ class Relay(
         fun fromUrl(address: String): Relay {
             return Relay(address)
         }
+
+        suspend fun fetchInfoFor(relayUrl: String, httpClient: HttpClient = httpClient()): Info {
+            val raw = StringBuilder(relayUrl).removePrefix("wss://").removePrefix("ws://")
+            val actualUrl = "https://$raw"
+            val relayInfoResponse = httpClient.get(actualUrl) {
+                headers.append("Accept", "application/nostr+json")
+            }
+
+            if (relayInfoResponse.status.isSuccess()) {
+                return infoFromJson(relayInfoResponse.bodyAsText())
+            }
+            else throw RelayInfoFetchError("Could not fetch relay info with reason: ${relayInfoResponse.bodyAsText()}")
+        }
+
+        fun infoFromJson(relayInfoJson: String): Info {
+            val relayInfo = eventMapper.decodeFromString<Info>(relayInfoJson)
+            return relayInfo
+        }
     }
 
     override fun toString(): String {
         return "Relay(url=$relayURI, read=$readPolicy, write=$writePolicy)"
     }
+
+    @Serializable
+    class Info(
+        val name: String = "",
+        val description: String = "",
+        val banner: String = "",
+        val icon: String = "",
+        val pubkey: String = "",
+        val contact: String = "",
+        @SerialName("supported_nips") val supportedNips: IntArray = emptyArray<Int>().toIntArray(),
+        @SerialName("software") val relaySoftware: String = "",
+        @SerialName("version") val softwareVersion: String = ""
+    )
 }
