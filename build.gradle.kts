c0ca35aeb0cb26f5da76e7bafc55e980bde6a6da
KotlinGeekDev <kotlingeek@protonmail.com>
2026-07-28 22:36:48 +0100

    Migrate build to version catalog. Fix issues along the way. Move publishing to core module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,80 +23,25 @@
  *
  */
 
-import com.vanniktech.maven.publish.SonatypeHost
 
 buildscript {
 
-    val kotlinVersion = "2.1.10"
-
     dependencies {
-        classpath(kotlin("gradle-plugin", version = kotlinVersion))
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath(kotlin("serialization", version = kotlinVersion))
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.gradle)
     }
 
 }
 
 plugins {
-    kotlin("multiplatform") version "2.1.10" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlinx.atomicfu") version "0.27.0"
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlinx.atomicfu)
+
 
 }
 
-
-allprojects {
-    apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
-    apply(plugin = "com.vanniktech.maven.publish")
-
-    val isJitpack = System.getenv("JITPACK") == "true"
-
-    group = "io.github.kotlingeekdev"
-    version = "1.0-beta-19"
-
-    mavenPublishing {
-        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-        if (!isJitpack){
-            signAllPublications()
-        }
-
-
-        coordinates(group.toString(), "rhodium", version.toString())
-
-//        configure(KotlinMultiplatform(
-//            javadocJar = JavadocJar.Javadoc(),
-//            sourcesJar = true
-//        ))
-
-        pom {
-            name = "Rhodium"
-            description = " A Kotlin Multiplatform library for Nostr"
-            url = "https://github.com/KotlinGeekDev/Rhodium"
-
-            licenses {
-                license {
-                    name = "The MIT License"
-                    url = "https://opensource.org/license/MIT"
-                    distribution = "https://opensource.org/license/MIT"
-                }
-            }
-
-            developers {
-                developer {
-                    name = "KotlinGeekDev"
-                    email = "kotlingeek@protonmail.com"
-                    url = "https://github.com/KotlinGeekDev"
-                }
-            }
-
-            scm {
-                connection = "scm:git:git://github.com/KotlinGeekDev/Rhodium.git"
-                url = "https://github.com/KotlinGeekDev/Rhodium"
-
-            }
-        }
-    }
-}
+group = "io.github.kotlingeekdev"
+version = "1.0-beta-19"
 
 
